45cbe087897cf1ddef7c1cb64dcbd13e2e458057
ManoharaMC <mcmanohar62@gmail.com>
2021-10-26 21:46:24 +0530

    first

---
+++ mnc.js
@@ -0,0 +1,10 @@
+$(".Click-here").on('click', function() {
+    $(".custom-model-main").addClass('model-open');
+  }); 
+  $(".close-btn, .bg-overlay").click(function(){
+    $(".custom-model-main").removeClass('model-open');
+  });
+  
+  
+  
+  
